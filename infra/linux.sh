c2d2d070444f54b96c599bffc07f3a8605b9b308
alvindimas05 <alvin.dimas.praditya@gmail.com>
2026-09-01 11:27:21 +0700

    ci: add rpm build

--- infra/linux.sh
+++ infra/linux.sh
@@ -6,7 +6,7 @@ echo "DISCORD_APPLICATION_ID=${DISCORD_APPLICATION_ID:-}" >> .env
 sudo curl --output-dir /etc/apt/trusted.gpg.d -O https://apt.fruit.je/fruit.gpg
 echo "deb http://apt.fruit.je/debian trixie mpv" | sudo tee /etc/apt/sources.list.d/fruit.list
 sudo apt-get update
-sudo apt-get install -y libwebkit2gtk-4.1-dev libappindicator3-dev librsvg2-dev patchelf librust-alsa-sys-dev libmpv-dev lld
+sudo apt-get install -y libwebkit2gtk-4.1-dev libappindicator3-dev librsvg2-dev patchelf librust-alsa-sys-dev libmpv-dev lld rpm
 
 pnpm i
 pnpm run init
